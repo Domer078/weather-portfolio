6e8d0bad50bff202f7eb4c3d0ce6f6f3602984b4
Domer078 <khomotsomabala05@gmail.com>
2026-07-14 15:25:57 +0200

    Added CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import "./weather.css";
+
 import Weather from "./weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
       <Weather />
     </div>
   );
--- src/weather.js
+++ src/weather.js
@@ -2,5 +2,17 @@ import React from "react";
 import axios from "axios";
 
 export default function Weather() {
-  return "hello";
+  return (
+    <div className="Weather">
+      <form>
+        <input
+          type="search"
+          placeholder="Enter a city.."
+          autoFocus="on"
+          className="searchInput"
+        />
+        <input type="submit" value="search" className="btn btn-primary p-10S" />
+      </form>
+    </div>
+  );
 }
