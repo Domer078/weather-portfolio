0d0b81ce8314293b71a36c2534c4b84390eba86d
Domer078 <khomotsomabala05@gmail.com>
2026-07-14 23:45:03 +0200

    Added Bootstrap's form-control class

--- src/weather.js
+++ src/weather.js
@@ -1,27 +1,30 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  <div className="Weather">
-    <div className="container">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              autoFocus={true}
-              className="searchInput"
-            />
+  return (
+    <div className="Weather">
+      <div className="container">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                autoFocus={true}
+                className="form-control searchInput"
+              />
+            </div>
+            <div className="col-3 p-0">
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3 p-0">
-            <input
-              type="submit"
-              value="search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>;
+  );
 }
