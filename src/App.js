e40195200fc8cf19a2dd889c34f38c255b19fd83
Domer078 <khomotsomabala05@gmail.com>
2026-07-16 16:30:58 +0200

    renamed my file name

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
   return (
     <div className="App">
       <Weather defaultCity="New York" />
+
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/domer078">Khomotso Mabala</a> and is
+        <a href="https://github.com/Domer078/weather-portfolio">
+          {" "}
+          open-sourced on GitHub
+        </a>{" "}
+        and <a>hosted on Netlify</a>
+      </footer>
     </div>
   );
 }
