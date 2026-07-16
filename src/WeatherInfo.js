fdba79004491f88ee4b8db42edcad227db80e908
Domer078 <khomotsomabala05@gmail.com>
2026-07-16 18:04:38 +0200

    Moved my footer to another component and added CSS

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -32,6 +32,30 @@ export default function WeatherInfo(props) {
           </div>
         </div>
       </div>
+      <footer>
+        {" "}
+        This project was coded by{" "}
+        <a href="https://github.com/domer078" target="_blank" rel="noreferrer">
+          Khomotso Mabala
+        </a>{" "}
+        and is
+        <a
+          href="https://github.com/Domer078/weather-portfolio"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://shimmering-kitsune-6975a6.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
