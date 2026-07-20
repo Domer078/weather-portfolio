4353a79b231349088bde186732b965e0e28d43b5
Domer078 <khomotsomabala05@gmail.com>
2026-07-20 21:39:34 +0200

    Resolve Netlify build error by refactoring forecast useEffect

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -8,42 +8,34 @@ export default function WeatherForecast(props) {
   const [forecast, setForecast] = useState(null);
 
   useEffect(() => {
-    if (props.coordinates) {
-      load();
-    }
-  }, [props.coordinates]);
-  function handleResponse(response) {
-    setForecast(response.data.daily);
-    setLoaded(true);
-  }
+    if (!props.coordinates) return;
 
-  function load() {
-    let apiKey = "63340413at53cc6c6ba7a81a11oc3f05";
-    let longitude = props.coordinates.longitude;
-    let latitude = props.coordinates.latitude;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+    setLoaded(false);
+
+    const apiKey = "63340413at53cc6c6ba7a81a11oc3f05";
+    const longitude = props.coordinates.longitude;
+    const latitude = props.coordinates.latitude;
+
+    const apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
-  }
-  if (loaded) {
-    return (
-      <div className="WeatherForecast">
-        <div className="row">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />
-                </div>
-              );
-            } else {
-              return null;
-            }
-          })}
-        </div>
-      </div>
-    );
-  } else {
+  }, [props.coordinates]);
+
+  if (!loaded) {
     return null;
   }
+
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        {forecast.map((dailyForecast, index) =>
+          index < 5 ? (
+            <div className="col" key={index}>
+              <WeatherForecastDay data={dailyForecast} />
+            </div>
+          ) : null,
+        )}
+      </div>
+    </div>
+  );
 }
